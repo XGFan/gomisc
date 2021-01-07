4c1d5320697619321efa5afba94fdf375ab53485
Guofan Xu <xuguofan@live.com>
2021-01-07 23:09:41 +0800

    init

---
+++ watcher/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	flag.Parse()
+	var ips []string
+	if len(flag.Args()) != 0 {
+		ips = flag.Args()
+	} else {
+		ips = []string{"192.168.2.1"}
+	}
+	log.Printf("Check %s\n", ips)
+	receivedCount := 0
+	for _, ip := range ips {
+		command := exec.Command("ping", "-c", "5", ip)
+		output, _ := command.Output()
+		s := string(output)
+		split := strings.Split(s, "\n")
+		for _, t := range split {
+			if strings.Contains(t, "received") && strings.Contains(t, "loss") {
+				log.Println(t)
+				i := strings.Split(strings.TrimSpace(strings.Split(t, ",")[1]), " ")
+				count, _ := strconv.Atoi(strings.TrimSpace(i[0]))
+				receivedCount += count
+				break
+			}
+		}
+	}
+	if receivedCount == 0 {
+		log.Println("It is Dead")
+		exec.Command("shutdown", "now").Start()
+	} else {
+		log.Println("It is Alive")
+	}
+}
